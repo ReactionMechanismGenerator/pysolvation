c9c65ac4efd486c5c3db22a7a6152c0a7a5873ce
Matt Johnson <mjohnson541@gmail.com>
2021-08-18 19:36:52 -0400

    add class for storing Solute information: identifiers and solute parameters

--- pysolvation/solvation/solute.py
+++ pysolvation/solvation/solute.py
@@ -1,1 +1,15 @@
+import pandas as pd
+
+class Solute:
+    """
+    Stores Solute identifiers and parameters
+    """
+    def __init__(self,smiles,inchi,E,S,A,B,L):
+        self.smiles = smiles
+        self.inchi = inchi
+        self.E = E
+        self.S = S
+        self.A = A
+        self.B = B
+        self.L = L
 
