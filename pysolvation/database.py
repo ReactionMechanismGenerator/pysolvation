34ef554e9fbe8c6d71844bcdf80cad1b14a5f903
Matt Johnson <mjohnson541@gmail.com>
2021-08-18 19:36:18 -0400

    add methods for pulling species objects out by inchi or smiles

--- pysolvation/database.py
+++ pysolvation/database.py
@@ -3,3 +3,15 @@ from rdkit import Chem
 class InchiKeyedDatabase:
     def __init__(self,spcs):
         self.spcs = spcs
+
+    def get_species_inchi(self,inchi):
+        for spc in self.spcs:
+            if spc.inchi == inchi:
+                return spc
+        else:
+            return None
+
+    def get_species_smiles(self,smiles):
+        mol = Chem.MolFromSmiles(smiles)
+        inchi = Chem.MolToInchi(mol, options='/FixedH')
+        return self.get_species_inchi(inchi)
