c75733b41b7d9485b3176f7c4ade248052be31a7
Matt Johnson <mjohnson541@gmail.com>
2021-08-18 19:39:14 -0400

    add COSMODatabase class for manipulating sets of COSMOSpecies objects

--- pysolvation/cosmo/database.py
+++ pysolvation/cosmo/database.py
@@ -11,3 +11,11 @@ class COSMOSpecies:
         self.smiles = smiles
         self.n_conf = n_conf
         self.path = path
+
+class COSMODatabase(InchiKeyedDatabase):
+    """
+    Database for holding COSMOSpecies objects
+    """
+    def __init__(self,spcs,level):
+        super().__init__(spcs)
+        self.level = level
