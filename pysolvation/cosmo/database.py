67c6e8ddac352603c530d9435c143e416548feec
Matt Johnson <mjohnson541@gmail.com>
2021-08-18 19:40:21 -0400

    add function for loading COSMOSpecies objects from a CSV

--- pysolvation/cosmo/database.py
+++ pysolvation/cosmo/database.py
@@ -19,3 +19,17 @@ class COSMODatabase(InchiKeyedDatabase):
     def __init__(self,spcs,level):
         super().__init__(spcs)
         self.level = level
+
+def database_summary_to_species(path):
+    """
+    Loads COSMOSpecies objects from a csv
+    """
+    db = pd.read_csv(path)
+    spcs =  []
+    for i in range(len(db)):
+        spcs.append(COSMOSpecies(name=db["cosmo name"][i],
+                                inchi=db["inchi"][i],
+                                smiles=db["smiles"][i],
+                                n_conf=db["number of conformers"][i],
+                                path=db["file path"][i]))
+    return spcs
