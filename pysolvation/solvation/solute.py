0a054d767630373c131044940c703ac42306d1b6
Matt Johnson <mjohnson541@gmail.com>
2021-08-18 19:37:18 -0400

    add function for loading Solute objects from a csv

--- pysolvation/solvation/solute.py
+++ pysolvation/solvation/solute.py
@@ -13,3 +13,19 @@ class Solute:
         self.B = B
         self.L = L
 
+def load_solutes(path):
+    """
+    Function for loading Solute objects from a csv
+    with the solute parameters and smiles and inchi identifiers
+    """
+    db = pd.read_csv(path)
+    spcs =  []
+    for i in range(len(db)):
+        spcs.append(Solute(smiles=db["smiles"][i],
+                                inchi=db["inchi"][i],
+                                E=db["E"][i],
+                                S=db["S"][i],
+                                A=db["A"][i],
+                                B=db["B"][i],
+                                L=db["L"][i]))
+    return spcs
